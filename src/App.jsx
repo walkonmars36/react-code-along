a1aacebfb7a898d3e8c68bbd378018f44989831c
Mark Lawson <mark@walkonmars.co.uk>
2022-06-06 10:35:16 +0100

    getting files ready

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// App.jsx
+import "./App.scss";
+
+const App = () => {
+  return (
+    <div className="app">
+      <header className="greeting">
+        <h1 className="greeting__heading">Hello World</h1>
+      </header>
+    </div>
+  );
+};
+
+export default App;
